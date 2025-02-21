d9ae4f23f76081e400a48504be042ff2214322e2
Joe Gribble <jgribble@adaptavist.com>
2025-02-21 14:15:15 +0000

    fixing 404 page and adding harribel assets

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
       <h1 className="text-4xl font-bold mb-4">
         <span className="text-red-600">4</span>04 Page Not Found
       </h1>
